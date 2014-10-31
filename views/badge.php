8241abed4497adfd8ed901575e5d644b92cc356d
Rohit Suthar <rohitsuthar@users.noreply.github.com>
2014-10-31 19:25:15 +0530

    google+ badge

---
+++ views/badge.php
@@ -0,0 +1,9 @@
+<div class="g-page" data-width="<?=$rendered['width'];?>" data-href="https://plus.google.com/114389194992381809226" data-theme="<?=$rendered['theme'];?>" data-layout="<?=$rendered['layout'];?>" data-showtagline="<?=$rendered['show_tagline'];?>" data-showcoverphoto="<?=$rendered['show_coverphoto'];?>" data-rel="publisher"></div>
+
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
